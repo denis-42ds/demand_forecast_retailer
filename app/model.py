9dfa3ad87b5cd556470b59f9559da5449932712c
denis-42ds <zaytseff.denis@gmail.com>
2023-10-01 08:55:27 +0300

    readme 'n files for docker added

---
+++ app/model.py
@@ -0,0 +1,7 @@
+import pickle
+import pandas as pd
+import numpy as np
+import sklearn
+
+model = pickle.load(open('model_cbr.pkl', 'rb'))
+
---
+++ ml_Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.10
+
+COPY app /app
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+CMD ["python", "app.py"]
