b5435790e8349c3492bb6c125eca7ab80cd642d5
denis-42ds <zaytseff.denis@gmail.com>
2023-10-04 15:49:51 +0300

    edit model.py

--- app/model.py
+++ app/model.py
@@ -5,3 +5,23 @@ import sklearn
 
 model = pickle.load(open('model_cbr.pkl', 'rb'))
 
+def forecast(sales: dict, item_info: dict, store_info: dict) -> list:
+    """
+    Функция для предсказания продаж
+    :params sales: исторические данные по продажам
+    :params item_info: характеристики товара
+    :params store_info: характеристики магазина
+
+    """
+    ans = model.predict(list)
+    print('answer', ans)
+    return ans
+    
+    
+    
+    
+    
+    #sales = [el["sales_units"] for el in sales]
+    #mean_sale = sum(sales) / len(sales)
+    #return [mean_sale] * 5
+
